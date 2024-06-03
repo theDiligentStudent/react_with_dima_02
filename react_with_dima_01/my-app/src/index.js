259ba703e4dc5f92f0794d43788b37ebf69612c8
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-04 00:33:55 +0300

    3 Commit. 7 Lesson 1 part. ---- 4/06/24 -- Повторил 7й Ур. Димы --- до 18й минуты

--- react_with_dima_01/my-app/src/index.js
+++ react_with_dima_01/my-app/src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    After root Посмотри Откуда *??* это Берётся?!!
     <App />
+    Before root Посмотри Откуда *??* это Берётся?!!
   </React.StrictMode>
 );
 
